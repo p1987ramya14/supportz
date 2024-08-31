095917673f9e536fe88520e4a651d495dd91de17
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-09-01 03:11:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-$finalLandingPageUrl = 'https://www.amazon.com/gp/product/B0CWS8MNW1/ref=as_li_tl?th=1';
-header('Location: ' . $finalLandingPageUrl);
+$finalLandingPageUrl = 'https://www.amazon.com/b/?node=116063164011';
+
+$referrerData = isset($_GET['referrer']) ? $_GET['referrer'] : '';
+
+$redirectUrl = $finalLandingPageUrl . (strpos($finalLandingPageUrl, '?') === false ? '?' : '&') . 'referrer=' . urlencode($referrerData);
+
+header('Location: ' . $redirectUrl);
 exit();
 ?>
