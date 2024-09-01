bf9f1e09f1ab312197b9b98c5cec3ecdacfeb9c5
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-09-01 20:24:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$finalLandingPageUrl = 'https://www.google.com';
+$finalLandingPageUrl = 'https://www.amazon.com/gp/product/B0CWS8MNW1/ref=as_li_tl?th=1';
 $referrerData = isset($_GET['referrer']) ? $_GET['referrer'] : '';
 $redirectUrl = $finalLandingPageUrl . (strpos($finalLandingPageUrl, '?') === false ? '?' : '&') . 'referrer=' . urlencode($referrerData);
 
