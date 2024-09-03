7b850480076cfa471a5c014b5a7cc2b672646bba
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-09-04 01:25:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$finalLandingPageUrl = 'https://amazon.com';
+$finalLandingPageUrl = 'https://www.letifly.com/collections/planters?srsltid=AfmBOortfFdsQdJolr_06CwAidsNth4T3UFfCKBpmQBq2ngmt9KZdCwJ';
 $referrerData = isset($_GET['referrer']) ? $_GET['referrer'] : '';
 $redirectUrl = $finalLandingPageUrl . (strpos($finalLandingPageUrl, '?') === false ? '?' : '&') . 'referrer=' . urlencode($referrerData);
 
