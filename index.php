70b20bca53ba51f3bb4513930bbdfdbfba2633bb
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-09-06 19:08:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 $finalLandingPageUrl = 'https://www.facebook.com/Google';
-header('Location: ' . $finalLandingPageUrl);
+$referrerData = isset($_GET['referrer']) ? $_GET['referrer'] : '';
+$redirectUrl = $finalLandingPageUrl . (strpos($finalLandingPageUrl, '?') === false ? '?' : '&') . 'referrer=' . urlencode($referrerData);
+
+header('Location: ' . $redirectUrl);
 exit();
 ?>
