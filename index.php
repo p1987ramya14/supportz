9d5a30f7594ff982949bdec7ca65ca6c519782b0
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-09-13 01:57:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     $gclid = isset($_GET['gclid']) ? $_GET['gclid'] : '';
 
     // Amazon URL
-    $amazon_url = https://www.wakefieldpeanutco.com/prod_detail_list/raw-peanuts';
+    $amazon_url = 'https://www.wakefieldpeanutco.com/prod_detail_list/raw-peanuts';
 
     // If gclid is present, append it to the Amazon URL
     if ($gclid) {
