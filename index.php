07dd78a36a02697c37d59425db45056e539779f8
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-09-17 22:05:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$landingPageUrl = 'https://www.amazon.com/deals';
+$landingPageUrl = 'https://www.amazon.com/deals/shop';
 $campaignId = isset($_GET['campaign_id']) ? $_GET['campaign_id'] : '';
 $pubId = isset($_GET['pub_id']) ? $_GET['pub_id'] : '';
 $url = isset($_GET['url']) ? $_GET['url'] : $landingPageUrl;
