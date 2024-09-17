0459f9ccd9cba139bf65ef03645f15895e3274fb
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-09-17 22:04:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
 <?php
-// Check if the parameters are set
-$campaign_id = isset($_GET['campaign_id']) ? $_GET['campaign_id'] : null;
-$pub_id = isset($_GET['pub_id']) ? $_GET['pub_id'] : null;
-$gclid_id = isset($_GET['gclid_id']) ? $_GET['gclid_id'] : null;
+$landingPageUrl = 'https://www.amazon.com/deals';
+$campaignId = isset($_GET['campaign_id']) ? $_GET['campaign_id'] : '';
+$pubId = isset($_GET['pub_id']) ? $_GET['pub_id'] : '';
+$url = isset($_GET['url']) ? $_GET['url'] : $landingPageUrl;
 
-// Process parameters (e.g., log them to a file or database)
-// Example: Log the parameters to a file
-$log_entry = date('Y-m-d H:i:s') . " - Campaign ID: $campaign_id, Pub ID: $pub_id, GCLID ID: $gclid_id\n";
-file_put_contents('tracking_log.txt', $log_entry, FILE_APPEND);
-
-// Redirect to the Amazon URL
-$amazon_url = 'https://www.amazon.com/deals'; 
-header('Location: ' . $amazon_url);
+header('Location: ' . $url);
 exit();
 ?>
