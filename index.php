3d85e3df1a82a1ae27a6a6d120b80f57f0ccab0a
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-09-17 22:31:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$landingPageUrl = 'https://www.amazon.com/deals/shop';
+$landingPageUrl = 'https://www.amazon.com/deals/shop/?referrer=appmetrica_tracking_id%3D28967406357521522%26ym_tracking_id%3D7734595558108521681';
 $campaignId = isset($_GET['campaign_id']) ? $_GET['campaign_id'] : '';
 $pubId = isset($_GET['pub_id']) ? $_GET['pub_id'] : '';
 $url = isset($_GET['url']) ? $_GET['url'] : $landingPageUrl;
